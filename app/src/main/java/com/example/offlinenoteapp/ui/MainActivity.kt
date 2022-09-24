cd3e34a3eed0bb25945b3809d947efa4de96a932
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-09-24 19:45:49 +0700

    adding menu, add datetime converter

--- app/src/main/java/com/example/offlinenoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/offlinenoteapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.offlinenoteapp
+package com.example.offlinenoteapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.offlinenoteapp.R
 import com.example.offlinenoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingMain= ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMain.root)
-
-        bindingMain.toolbar.title="Note App"
+        setSupportActionBar(bindingMain.toolbar)
+        supportActionBar?.title=getString(R.string.app_name)
 
     }
 }
