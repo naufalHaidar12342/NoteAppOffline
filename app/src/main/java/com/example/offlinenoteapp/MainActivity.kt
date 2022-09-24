be04e39ed406b2986d54627d3128eceacb07b84b
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-09-24 11:22:45 +0700

    upgrade compileSdk and targetSdk to 32, add viewbinding to MainActivity

--- app/src/main/java/com/example/offlinenoteapp/MainActivity.kt
+++ app/src/main/java/com/example/offlinenoteapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.offlinenoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.offlinenoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMain:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        bindingMain.toolbar.title="Note App"
+
     }
 }
